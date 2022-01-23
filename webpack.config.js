f4a1a77967411e6cf9466c69d1d827116acaa1f8
ndresgarc <email@andresgarcia.dev>
2022-01-23 11:12:59 +0100

    chore: add webpack to build process

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+
+    mode: 'development',
+
+  entry: {
+    "trims.global.prefix": "./src/trims.global.prefix.ts"
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+
+  // devtool: 'inline-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+  
+};
